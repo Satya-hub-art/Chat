3a6f683d5e900e5f5b54ae6a11b9bbea9e6750b2
Vivo <vivorealme2023@gmail.com>
2025-11-21 14:25:54 +0530

    Fix initializeApp syntax with databaseURL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://chatapp-b5d7d-default-rtdb.asia-southeast1.firebasedatabase.app"  // <-- Paste your URL
 });
 
